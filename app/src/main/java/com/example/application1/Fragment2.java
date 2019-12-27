a35793156ca73cb794868928b1145173b1432c6f
jhg6636 <gusrbwjd1229@gmail.com>
2019-12-27 13:37:26 +0900

    contact link added

--- app/src/main/java/com/example/application1/Fragment2.java
+++ app/src/main/java/com/example/application1/Fragment2.java
@@ -6,6 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
+
+import com.handstudio.android.vo.Contact;
 
 public class Fragment2 extends Fragment {
 
