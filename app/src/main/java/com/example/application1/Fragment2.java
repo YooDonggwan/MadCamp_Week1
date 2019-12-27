867983edd9110d2c384b4e1a4d9ec192ea42dd20
YooDonggwan <dbehdrhks@naver.com>
2019-12-27 19:28:24 +0900

    1227

--- app/src/main/java/com/example/application1/Fragment2.java
+++ app/src/main/java/com/example/application1/Fragment2.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment2 extends Fragment {
+
 
     public Fragment2(){
 
@@ -15,6 +17,8 @@ public class Fragment2 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+
         return inflater.inflate(R.layout.fragment_fragment2, container, false);
     }
 }
